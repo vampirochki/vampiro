3502704fd3bf89b0c257857b1c25875ba6e0f637
vampirochki <vampirochki@gmail.com>
2025-04-11 20:07:11 +0200

    1

--- main.py
+++ main.py
@@ -1,1 +1,2 @@
-1
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QListWidget, QLineEdit, QTextEdit, QInputDialog, QHBoxLayout, QVBoxLayout, QFormLayout
