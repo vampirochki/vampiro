036e1fc0e16e11b0f274999287faa972df78f53b
vampirochki <vampirochki@gmail.com>
2025-04-11 20:16:07 +0200

    ui

---
+++ ui.py
@@ -0,0 +1,77 @@
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QListWidget, QLineEdit, QTextEdit, QInputDialog, QHBoxLayout, QVBoxLayout, QFormLayout
+
+
+import json
+
+
+app = QApplication([])
+
+
+'''Інтерфейс програми'''
+# параметри вікна програми
+notes_win = QWidget()
+notes_win.setWindowTitle('Розумні замітки')
+notes_win.resize(900, 600)
+
+
+# віджети вікна програми
+list_notes = QListWidget()
+list_notes_label = QLabel('Список заміток')
+
+
+button_note_create = QPushButton('Створити замітку') # з'являється вікно з полем "Введіть ім'я замітки"
+button_note_del = QPushButton('Видалити замітку')
+button_note_save = QPushButton('Зберегти замітку')
+
+
+field_tag = QLineEdit('')
+field_tag.setPlaceholderText('Введіть тег...')
+field_text = QTextEdit()
+button_tag_add = QPushButton('Додати до замітки')
+button_tag_del = QPushButton('Відкріпити від замітки')
+button_tag_search = QPushButton('Шукати замітки по тегу')
+list_tags = QListWidget()
+list_tags_label = QLabel('Список тегів')
+
+
+# розташування віджетів по лейаутах
+layout_notes = QHBoxLayout()
+col_1 = QVBoxLayout()
+col_1.addWidget(field_text)
+
+
+col_2 = QVBoxLayout()
+col_2.addWidget(list_notes_label)
+col_2.addWidget(list_notes)
+row_1 = QHBoxLayout()
+row_1.addWidget(button_note_create)
+row_1.addWidget(button_note_del)
+row_2 = QHBoxLayout()
+row_2.addWidget(button_note_save)
+col_2.addLayout(row_1)
+col_2.addLayout(row_2)
+
+
+col_2.addWidget(list_tags_label)
+col_2.addWidget(list_tags)
+col_2.addWidget(field_tag)
+row_3 = QHBoxLayout()
+row_3.addWidget(button_tag_add)
+row_3.addWidget(button_tag_del)
+row_4 = QHBoxLayout()
+row_4.addWidget(button_tag_search)
+
+
+col_2.addLayout(row_3)
+col_2.addLayout(row_4)
+
+
+layout_notes.addLayout(col_1, stretch = 2)
+layout_notes.addLayout(col_2, stretch = 1)
+notes_win.setLayout(layout_notes)
+
+
+# запуск програми
+notes_win.show()
+app.exec_()
